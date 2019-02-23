77f999895272d82885b156596047220d282ab5f8
Pedro Pinto <pedropinto.web@gmail.com>
2019-02-23 01:55:45 +0000

    Config app, routes and tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
+import { RouterModule } from "@angular/router"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { NavbarComponent } from "./navbar/navbar.component"
+import { PageContactsComponent } from "./page-contacts/page-contacts.component"
+import { PageHomeComponent } from "./page-home/page-home.component"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		PageHomeComponent,
+		PageContactsComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		RouterModule.forRoot([
+			{ path: "", component: PageHomeComponent },
+			{ path: "contactos", component: PageContactsComponent }
+		])
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
