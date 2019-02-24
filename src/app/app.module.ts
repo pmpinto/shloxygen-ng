138bf47f610ab5676f2baedf851b7721f3ac7b78
Pedro Pinto <pedropinto.web@gmail.com>
2019-02-24 00:40:50 +0000

    Add main site structure
    Components: Header, Footer
    Also adds SCSS and linting configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { NavbarComponent } from "./navbar/navbar.component"
 import { PageContactsComponent } from "./page-contacts/page-contacts.component"
-import { PageHomeComponent } from "./page-home/page-home.component"
+import { PageHomeComponent } from "./page-home/page-home.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavbarComponent,
 		PageHomeComponent,
-		PageContactsComponent
+		PageContactsComponent,
+		HeaderComponent,
+		FooterComponent
 	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
